e41f469f61854274b12a8b78727729e3617038f4
fajrijuanda <if22.fajrijuanda@mhs.ubpkarawang.ac.id>
2024-07-06 13:15:22 +0700

    update
    Co-authored-by: Ahmad Ludfi Ikhwana <Dfilxy@users.noreply.github.com>
    Co-authored-by: MiNerVa <iMiNerVaa@users.noreply.github.com>

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -1,189 +1,328 @@
 <?php
-namespace App\Http\Controllers\Web\Auth;
+
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\SendOtpMail;
 use App\Models\User;
+use App\Models\Cart;
+use App\Models\Wishlist;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\EmailOtp;
-use App\Mail\ResetPassword;
+use Carbon\Carbon;
+use Exception;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Twilio\Rest\Client;
+
 class AuthenticationController extends Controller
 {
-    public function showLoginForm()
+    protected $twilio;
+
+    public function __construct()
     {
-        return view('admin.authentication.login');
+        $this->twilio = new Client(env('TWILIO_SID'), env('TWILIO_AUTH_TOKEN'));
     }
+
+     public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+            'phone_number' => 'sometimes|string',
+            'avatar' => 'sometimes|image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 422);
+        }
+
+        $validated = $validator->validate();
+
+        // Kirim OTP ke email
+        $otp = rand(100000, 999999);
+        $this->sendEmailOTP($validated['email'], $otp);
+
+        // Simpan data pengguna sementara dengan status "Pending"
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+            'phone_number' => $validated['phone_number']?? null,
+            'avatar' => $validated['avatar'] ?? 'https://swiftyleshop.com/assets/images/dashboard/1.png',
+            'role' => 'Customer',
+            'gender' => 'Other',
+            'phone_verified' => 'No',
+            'email_verified' => 'No',
+            'status' => 'Pending',
+            'email_otp' => $otp,
+        ]);
+
+        return response()->json([
+            'message' => 'OTP sent to your email. Please verify.',
+            'user_id' => $user->id
+        ], 201);
+    }
+
+    protected function sendEmailOTP($email, $otp)
+    {
+        // Kirim email menggunakan Laravel mail
+        Mail::to($email)->send(new SendOtpMail($otp));
+    }
+
+    public function confirmEmailOTP(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'otp' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 422);
+        }
+
+        $validated = $validator->validate();
+        $user = User::find($validated['user_id']);
+
+        if ($user->email_otp == $validated['otp']) {
+            $user->email_verified = 'Yes';
+            $user->email_otp = null;
+            $user->email_verified_at = Carbon::now();
+            $user->status = 'Active';
+            $user->save();
+
+            // Generate JWT token
+            $token = $this->generateToken($user);
+
+            return response()->json([
+                'message' => 'Email verified successfully.',
+                'user_id' => $user->id,
+                'token' => $token
+            ], 200);
+        } else {
+            return response()->json(['error' => 'Invalid OTP'], 422);
+        }
+    }
+
+
+    public function inputPhoneNumber(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'phone_number' => 'required|string|min:10'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 422);
+        }
+
+        $validated = $validator->validate();
+        $user = User::find($validated['user_id']);
+
+        // Kirim OTP ke nomor telepon
+        $otp = rand(100000, 999999);
+        $this->sendPhoneOTP($validated['phone_number'], $otp);
+
+        // Update nomor telepon dan simpan OTP
+        $user->phone_number = $validated['phone_number'];
+        $user->phone_otp = $otp;
+        $user->save();
+
+        return response()->json([
+            'message' => 'OTP sent to your phone. Please verify.',
+            'user_id' => $user->id
+        ], 200);
+    }
+
+    protected function sendPhoneOTP($phoneNumber, $otp)
+    {
+        $message = "Your verification code is: $otp";
+
+        try {
+            $this->twilio->messages->create("whatsapp:$phoneNumber", [
+                'from' => env('TWILIO_WHATSAPP_SANDBOX_NUMBER'),
+                'body' => $message
+            ]);
+
+            return response()->json([
+                'message' => 'OTP sent to WhatsApp. Please verify.'
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function confirmPhoneOTP(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|exists:users,id',
+            'otp' => 'required|numeric'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 422);
+        }
+
+        $validated = $validator->validate();
+        $user = User::find($validated['user_id']);
+
+        if ($user->phone_otp == $validated['otp']) {
+            $user->phone_verified = 'Yes';
+            $user->status = 'Active';
+            $user->phone_otp = null; // Hapus OTP setelah verifikasi
+            $user->phone_verified_at = Carbon::now();
+            $user->save();
+
+            // Generate JWT token
+            $token = $this->generateToken($user);
+
+            return response()->json([
+                'message' => 'Phone number verified successfully',
+                'user' => $user,
+                'token' => $token
+            ], 200);
+        } else {
+            return response()->json(['error' => 'Invalid OTP'], 422);
+        }
+    }
+
+
 
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->messages()], 422);
+        }
+
+        $credentials = $validator->validated();
+
+        try {
+            if (!Auth::attempt($credentials)) {
+                return response()->json(["error" => "Credentials not found"], 422);
+            }
 
             $user = Auth::user();
 
-            Session::put('name', $user->name);
-            Session::put('username', $user->username);
-            Session::put('email', $user->email);
-            Session::put('phone', $user->phone);
-            Session::put('gender', $user->gender);
-            Session::put('password', $user->password);
-            Session::put('role', $user->role);
-            Session::put('avatar', $user->avatar);
+            // Generate JWT token
+            $token = $this->generateToken($user);
 
-            return redirect()->intended('/dashboard');
-        }
+            // Create or update cart for Customer role
+            if (strtolower($user->role) === 'customer') {
+                $this->createOrUpdateCart($user);
+                $this->createOrUpdateWishlist($user);
+            }
 
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
+            return response()->json([
+                'id' => $user->id,
+                'name' => $user->name,
+                'role' => $user->role,
+                'bearer' => $token
+            ], 200);
 
-    public function showRegistrationForm()
-    {
-        return view('admin.authentication.sign-up');
-    }
-
-    public function register(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:5',
-        ]);
-
-        $otp = rand(100000, 999999); // Generate a 6-digit OTP
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'role' => 'admin',
-            'password' => Hash::make($request->password),
-            'email_otp' => $otp,
-            'email_verified'=>'No'
-        ]);
-
-        Mail::to($user->email)->send(new EmailOtp($otp));
-
-        return redirect()->route('verify.email.otp.form')->with('success', 'Registration successful. Please check your email for the OTP.');
-    }
-
-    public function showVerifyOtpForm()
-    {
-        return view('admin.authentication.verify-otp');
-    }
-
-    public function verifyOtp(Request $request)
-    {
-        $request->validate([
-            'otp' => 'required|digits:6',
-        ]);
-
-        $user = User::where('email_otp', $request->otp)->first();
-
-        if ($user) {
-            $user->email_verified = true;
-            $user->email_verified_at = now();
-            $user->email_otp = null;
-            $user->email_verified ='Yes';
-            $user->save();
-
-            return redirect()->route('login')->with('success', 'Email verified successfully. Please login.');
-        } else {
-            return back()->withErrors(['error' => 'Invalid OTP or email.']);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Authentication error: ' . $e->getMessage()], 500);
         }
     }
-    
 
-
-    public function showForgetPasswordForm()
+    protected function createOrUpdateCart($user)
     {
-        $data['title'] = 'Forget Password';
-        return view('admin.authentication.forget-password', $data);
+        // Find existing cart or create new if not exists
+        $cart = Cart::where('user_id', $user->id)->first();
+
+        if (!$cart) {
+            Cart::create([
+                'user_id' => $user->id,
+                'app_coupon_id' => null,
+                'total_discount' => 0,
+                'total_price' => 0,
+            ]);
+        }
     }
 
-    /**
-     * Handle forget password request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function forgetPassword(Request $request)
+    protected function createOrUpdateWishlist($user)
     {
-        $request->validate([
-            'email' => 'required|email',
+        // Find existing wishlist or create new if not exists
+        $wishlist = Wishlist::where('user_id', $user->id)->first();
+
+        if (!$wishlist) {
+            Wishlist::create([
+                'user_id' => $user->id,
+            ]);
+        }
+    }
+
+    public function refresh(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'refresh_token' => 'required|string'
         ]);
 
-        $user = User::where('email', $request->email)->first();
-
-        if (!$user) {
-            return back()->withErrors(['email' => 'Email not found.']);
+        if ($validator->fails()) {
+            return response()->json(['error' => 'Invalid refresh token'], 422);
         }
 
-        // Generate a random 6-digit OTP
-        $otp = rand(100000, 999999);
+        $refreshToken = $request->refresh_token;
 
-        // Update user's email_otp field
-        $user->email_otp = $otp;
-        $user->save();
+        try {
+            // Decode refresh token
+            $decoded = JWT::decode($refreshToken, new Key(env('JWT_SECRET_KEY'), 'HS256'));
 
-        // Send email with OTP
-        Mail::to($user->email)->send(new ResetPassword($otp));
+            // Validate the token
+            if ($decoded->exp < time()) {
+                return response()->json(['error' => 'Refresh token expired'], 401);
+            }
 
-        return redirect()->route('reset-password')->with('success', 'An OTP has been sent to your email for password reset.');
-    }
+            // Find the user based on token data
+            $user = User::find($decoded->sub);
 
+            if (!$user) {
+                return response()->json(['error' => 'User not found'], 404);
+            }
 
-    /**
-     * Show the reset password form.
-     *
-     * @return \Illuminate\View\View
-     */
-    public function showResetPasswordForm()
-    {
-        $data['title'] = 'Reset Password';
-        return view('admin.authentication.reset-password', $data);
-    }
+            // Generate new access token
+            $accessToken = $this->generateToken($user);
 
-    /**
-     * Handle reset password request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function resetPassword(Request $request)
-    {
-        $request->validate([
-            'otp' => 'required|digits:6',
-            'password' => 'required|string|min:5|confirmed',
-        ]);
+            return response()->json([
+                'access_token' => $accessToken,
+                'token_type' => 'bearer',
+                'expires_in' => Carbon::now()->addHours(1)->timestamp // Adjust as needed
+            ], 200);
 
-        $user = User::where('email_otp', $request->otp)->first();
-
-        if (!$user) {
-            return back()->withErrors(['otp' => 'Invalid OTP.']);
+        } catch (ExpiredException $e) {
+            return response()->json(['error' => 'Refresh token expired'], 401);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Invalid token'], 401);
         }
-
-        // Reset password
-        $user->password = Hash::make($request->password);
-        $user->email_otp = null; // Clear OTP after reset
-        $user->save();
-
-        return redirect()->route('login')->with('success', 'Password reset successfully. Please login with your new password.');
     }
+
     public function logout(Request $request)
     {
+        // Invalidate the token and log out the user
         Auth::logout();
 
-        $request->session()->invalidate();
-        $request->session()->regenerateToken();
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
 
-        return redirect('/');
+    protected function generateToken($user)
+    {
+        $payload = [
+            'sub' => $user->id,
+            'name' => $user->name,
+            'role' => $user->role,
+            'iat' => Carbon::now()->timestamp,
+            'exp' => Carbon::now()->addHours(24)->timestamp // Access token expiration time
+        ];
+
+        return JWT::encode($payload, env('JWT_SECRET_KEY'), 'HS256');
     }
 }
--- app/Http/Controllers/Web/Auth/AuthenticationController.php
+++ app/Http/Controllers/Web/Auth/AuthenticationController.php
@@ -1,6 +1,4 @@
 <?php
-
-// App/Http/Controllers/Web/AuthenticationController.php
 namespace App\Http\Controllers\Web\Auth;
 
 use App\Http\Controllers\Controller;
@@ -10,26 +8,16 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\ValidationException;
-
+use Illuminate\Support\Facades\Mail;
+use App\Mail\EmailOtp;
+use App\Mail\ResetPassword;
 class AuthenticationController extends Controller
 {
-    /**
-     * Show the login form.
-     *
-     * @return \Illuminate\View\View
-     */
     public function showLoginForm()
     {
         return view('admin.authentication.login');
     }
 
-    /**
-     * Handle login request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\RedirectResponse
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
@@ -56,57 +44,139 @@ class AuthenticationController extends Controller
         ]);
     }
 
-    /**
-     * Show the registration form.
-     *
-     * @return \Illuminate\View\View
-     */
     public function showRegistrationForm()
     {
-        return view('auth.register');
+        return view('admin.authentication.sign-up');
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:5',
+        ]);
+
+        $otp = rand(100000, 999999); // Generate a 6-digit OTP
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'role' => 'admin',
+            'password' => Hash::make($request->password),
+            'email_otp' => $otp,
+            'email_verified'=>'No'
+        ]);
+
+        Mail::to($user->email)->send(new EmailOtp($otp));
+
+        return redirect()->route('verify.email.otp.form')->with('success', 'Registration successful. Please check your email for the OTP.');
+    }
+
+    public function showVerifyOtpForm()
+    {
+        return view('admin.authentication.verify-otp');
+    }
+
+    public function verifyOtp(Request $request)
+    {
+        $request->validate([
+            'otp' => 'required|digits:6',
+        ]);
+
+        $user = User::where('email_otp', $request->otp)->first();
+
+        if ($user) {
+            $user->email_verified = true;
+            $user->email_verified_at = now();
+            $user->email_otp = null;
+            $user->email_verified ='Yes';
+            $user->save();
+
+            return redirect()->route('login')->with('success', 'Email verified successfully. Please login.');
+        } else {
+            return back()->withErrors(['error' => 'Invalid OTP or email.']);
+        }
+    }
+    
+
+
+    public function showForgetPasswordForm()
+    {
+        $data['title'] = 'Forget Password';
+        return view('admin.authentication.forget-password', $data);
     }
 
     /**
-     * Handle registration request.
+     * Handle forget password request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      * @throws \Illuminate\Validation\ValidationException
      */
-    public function register(Request $request)
+    public function forgetPassword(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'sometimes|string|email|max:255|unique:users',
-            'phone' => 'sometimes|string',
-            'role' => 'required|enum|default:Admin',
-            'gender' => 'required|enum|default:Other',
-            'password' => 'required|string|min:8',
+            'email' => 'required|email',
         ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'role' => $request->role,
-            'gender' => $request->gender,
-            'password' => Hash::make($request->password),
-        ]);
+        $user = User::where('email', $request->email)->first();
 
-        if ($user) {
-            Auth::login($user);
-            return redirect()->route('dashboard');  // Ganti 'home' dengan nama rute beranda Anda
-        } else {
-            return back()->withInput()->withErrors(['error' => 'Registration failed. Please try again.']);
+        if (!$user) {
+            return back()->withErrors(['email' => 'Email not found.']);
         }
+
+        // Generate a random 6-digit OTP
+        $otp = rand(100000, 999999);
+
+        // Update user's email_otp field
+        $user->email_otp = $otp;
+        $user->save();
+
+        // Send email with OTP
+        Mail::to($user->email)->send(new ResetPassword($otp));
+
+        return redirect()->route('reset-password')->with('success', 'An OTP has been sent to your email for password reset.');
+    }
+
+
+    /**
+     * Show the reset password form.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function showResetPasswordForm()
+    {
+        $data['title'] = 'Reset Password';
+        return view('admin.authentication.reset-password', $data);
     }
 
     /**
-     * Log the user out of the application.
+     * Handle reset password request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'otp' => 'required|digits:6',
+            'password' => 'required|string|min:5|confirmed',
+        ]);
+
+        $user = User::where('email_otp', $request->otp)->first();
+
+        if (!$user) {
+            return back()->withErrors(['otp' => 'Invalid OTP.']);
+        }
+
+        // Reset password
+        $user->password = Hash::make($request->password);
+        $user->email_otp = null; // Clear OTP after reset
+        $user->save();
+
+        return redirect()->route('login')->with('success', 'Password reset successfully. Please login with your new password.');
+    }
     public function logout(Request $request)
     {
         Auth::logout();
